a3b8eb6251faa69be9ff6296d382e8d09fd1202d
paulfuentesa-duoc <paul.fuentesa@profesor.duoc.cl>
2026-04-22 20:35:20 +0000

    Version 3: Parte recoleccion de datos

--- pipeline.py
+++ pipeline.py
@@ -17,15 +17,27 @@ def run_orchestator():
 
     print("--- Lectura del clima en tiempo real")
     total_lecturas=[]
-
+    
+    # We take 3 snapshots to simulate a stream
     for i in range(5):
-        df_lectura=leer_clima_tiempo_real()
-        if not df_lectura.empty:
-            total_lecturas.append(df_lectura)
-        time.sleep(5)
+        print(f"  > instantanea {i+1}...")
+        df_snap = leer_clima_tiempo_real()
+        if not df_snap.empty:
+            total_lecturas.append(df_snap)
+        time.sleep(1) # Short delay
     
     if total_lecturas:
-        almacen_datos['clima']=pd.concat(total_lecturas,ignore_index=True)
+        almacen_datos['clima'] = pd.concat(total_lecturas, ignore_index=True)
+    else:
+        almacen_datos['clima'] = pd.DataFrame()
+
+    for elemento, df in almacen_datos.items():
+        print(f"\n📍 FUENTE: {elemento}")
+        if not df.empty:
+            print(f"Rows: {len(df)} | Columns: {list(df.columns)}")
+            print(df.head(2))
+        else:
+            print("Empty Table (Check connection)")
 
     return almacen_datos
 
